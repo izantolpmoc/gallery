095692d147b8d185a0100c86e5492d2e9e32caf3
Naomie <naomie.discala@gmail.com>
2024-11-13 19:47:03 +0900

    updated description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Les photos du voyage au Japon en Août 2024."
           />
           <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Les photos du voyage au Japon en Août 2024."
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="Photos du Japon - Août 2024" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta name="twitter:title" content="Photos du Japon - Août 2024" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Les photos du voyage au Japon en Août 2024."
           />
         </Head>
         <body className="bg-black antialiased">
